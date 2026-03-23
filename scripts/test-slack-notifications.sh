1823c762ba059b2100bfa24be7660ddae23e3fb3
ishan-krishna-okta <ishan.krishna@okta.com>
2026-03-23 21:21:43 +0530

    Testing slack notif

--- scripts/test-slack-notifications.sh
+++ scripts/test-slack-notifications.sh
@@ -4,17 +4,21 @@ export SLACK_CHANNEL='#tmp-test-slack-notif'
 export SLACK_CHANNEL_PERSONAL='@ishan.krishna'
 export SLACK_CHANNEL_PERSONAL2='@Ishan Krishna'
 
-send_slack_message "${SLACK_CHANNEL}" \
-    ":white_check_mark:" \
-    "Author: \n GH:  \n Bacon:"\
-    "good"
+echo "=== All Environment Variables ==="
+env | sort
+echo "================================="
 
-send_slack_message "${SLACK_CHANNEL_PERSONAL}" \
-    ":white_check_mark:" \
-    "Author2: \n GH:  \n Bacon:"\
-    "good"
+# send_slack_message "${SLACK_CHANNEL}" \
+#     ":white_check_mark:" \
+#     "Author: \n GH:  \n Bacon:"\
+#     "good"
 
-send_slack_message "${SLACK_CHANNEL_PERSONAL2}" \
-    ":white_check_mark:" \
-    "Author3: \n GH:  \n Bacon:"\
-    "good"
+# send_slack_message "${SLACK_CHANNEL_PERSONAL}" \
+#     ":white_check_mark:" \
+#     "Author2: \n GH:  \n Bacon:"\
+#     "good"
+
+# send_slack_message "${SLACK_CHANNEL_PERSONAL2}" \
+#     ":white_check_mark:" \
+#     "Author3: \n GH:  \n Bacon:"\
+#     "good"
