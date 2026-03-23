5f69b62d1d94afb45815d38a349d17083afe092f
ishan-krishna-okta <ishan.krishna@okta.com>
2026-03-23 11:46:14 +0530

    Testing slack notif

--- scripts/test-slack-notifications.sh
+++ scripts/test-slack-notifications.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e
 export SLACK_CHANNEL='#tmp-test-slack-notif'
-echo "1"
-send_slack_message \"'@Ishan Krishna'\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
-echo "2"
-send_slack_message \"'#test-ci-notify-alias'\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
-echo "3"
-send_slack_message \"#test-ci-notify-alias\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
-echo "4"
-send_slack_message \"'@ishan.krishna'\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
-send_slack_message \"'#tmp-test-slack-notif'\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
-echo "3"
-send_slack_message \"#tmp-test-slack-notif\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
-
 
 send_slack_message "${SLACK_CHANNEL}" \
     ":white_check_mark:" \
