a4308f8f55f9afd65053cffb04039ab85a169d6f
ishan-krishna-okta <ishan.krishna@okta.com>
2026-03-23 11:32:14 +0530

    Testing slack notif

--- scripts/test-slack-notifications.sh
+++ scripts/test-slack-notifications.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
-send_slack_message \"@Ishan Krishna\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
+echo "1"
+send_slack_message \"'@Ishan Krishna'\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
+echo "2"
+send_slack_message \"'#test-ci-notify-alias'\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
+echo "3"
+send_slack_message \"#test-ci-notify-alias\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
+echo "4"
+send_slack_message \"'@ishan.krishna'\" \"MY_TASK\" \"Uh oh\" \"danger\" \"high\" \"Additional plain text message\"
