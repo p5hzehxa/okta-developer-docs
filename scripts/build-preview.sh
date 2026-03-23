0da80c54611bd3891c2d6a422f62df7e78db404a
ishan-krishna-okta <ishan.krishna@okta.com>
2026-03-23 10:33:31 +0530

    Testing setup service

--- scripts/build-preview.sh
+++ scripts/build-preview.sh
@@ -11,6 +11,17 @@ if [ -z "$NETLIFY_AUTH_TOKEN" ] || [ -z "$NETLIFY_SITE_ID" ]; then
   exit 1
 fi
 
+Xvfb :99 -screen 0 1366x768x16 &
+
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
+export DISPLAY=:99.0
+
+yum update -y
+yum -y install gtk2-2.24* xorg-x11-server-Xvfb libXtst* libXScrnSaver* GConf2* alsa-lib* gtk3
+
+setup_service node v16.14.0
+setup_service yarn 1.21.1
+
 # echo "Installing NVM..."
 
 # export NVM_DIR="$HOME/.nvm"
@@ -21,22 +32,9 @@ fi
 
 # [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-yum install -y gnupg2
+# echo "Installing Node 16..."
 
-echo "Installing Node 16..."
-# nvm install 16
-# nvm use 16
-
-setup_service node v16.20.2
-setup_service yarn 1.21.1
-
-echo "Node version:"
-node -v
-echo "NPM version:"
-npm -v
-
-echo "Yarn version:"
-yarn -v
+# setup_service node v16.20.2
 
 # echo "Installing Yarn..."
 # npm install -g yarn
