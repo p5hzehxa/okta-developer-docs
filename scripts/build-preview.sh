6561d784b83d6f3644a2ff4b950d63503a2363fc
ishan-krishna-okta <ishan.krishna@okta.com>
2026-03-23 10:40:10 +0530

    Testing setup service

--- scripts/build-preview.sh
+++ scripts/build-preview.sh
@@ -11,17 +11,6 @@ if [ -z "$NETLIFY_AUTH_TOKEN" ] || [ -z "$NETLIFY_SITE_ID" ]; then
   exit 1
 fi
 
-Xvfb :99 -screen 0 1366x768x16 &
-
-export DBUS_SESSION_BUS_ADDRESS=/dev/null
-export DISPLAY=:99.0
-
-yum update -y
-yum -y install gtk2-2.24* xorg-x11-server-Xvfb libXtst* libXScrnSaver* GConf2* alsa-lib* gtk3
-
-setup_service node v16.14.0
-setup_service yarn 1.21.1
-
 # echo "Installing NVM..."
 
 # export NVM_DIR="$HOME/.nvm"
@@ -32,12 +21,12 @@ setup_service yarn 1.21.1
 
 # [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-# echo "Installing Node 16..."
+echo "Installing Node 16..."
 
-# setup_service node v16.20.2
+setup_service node v16.20.2
 
-# echo "Installing Yarn..."
-# npm install -g yarn
+echo "Installing Yarn..."
+npm install -g yarn
 
 echo "Installing dependencies..."
 yarn install --frozen-lockfile --ignore-platform
