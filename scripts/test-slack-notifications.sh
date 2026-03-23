9776256bfcc79db7f5c4ff77cd33b0d682ab5437
ishan-krishna-okta <ishan.krishna@okta.com>
2026-03-23 11:52:09 +0530

    Testing slack notif

--- scripts/test-slack-notifications.sh
+++ scripts/test-slack-notifications.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e
 export SLACK_CHANNEL='#tmp-test-slack-notif'
+export SLACK_CHANNEL_PERSONAL='@ishan.krishna'
+export SLACK_CHANNEL_PERSONAL2='@Ishan Krishna'
 
 send_slack_message "${SLACK_CHANNEL}" \
     ":white_check_mark:" \
     "Author: \n GH:  \n Bacon:"\
+    "good"
+
+send_slack_message "${SLACK_CHANNEL_PERSONAL}" \
+    ":white_check_mark:" \
+    "Author2: \n GH:  \n Bacon:"\
+    "good"
+
+send_slack_message "${SLACK_CHANNEL_PERSONAL2}" \
+    ":white_check_mark:" \
+    "Author3: \n GH:  \n Bacon:"\
     "good"
