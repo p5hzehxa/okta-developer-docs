11886de526d3211eb07a3b1c0621e99f276ea274
ishan-krishna-okta <ishan.krishna@okta.com>
2026-03-24 11:14:19 +0530

    Testing slack notif

--- scripts/test-slack-notifications.sh
+++ scripts/test-slack-notifications.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+
+export SHA_LINK="https://github.com/okta/okta-developer-docs/commit/${SHA}"
+export BACON_LINK="https://bacon-go.aue1e.saasure.net/commits?artifact=okta-developer-docs&sha=${SHA}"
+
 if [[ -n "$AUTHOR" ]]; then
   AUTHOR_USERNAME="${AUTHOR%@*}"
   export AUTHOR_SLACK_HANDLE="@${AUTHOR_USERNAME}"
@@ -16,6 +20,6 @@ env | sort
 echo "================================="
 
 send_slack_message "${AUTHOR_SLACK_HANDLE}" \
-    "Preview for your topic branch ${BRANCH} is ready :white_check_mark:" \
-    "Preview: ${PREVIEW_URL} \n GH:  \n Bacon: <${BACON_LINK}|${SHA}>"\
+    "Preview for your topic branch \"${BRANCH}\" is ready :white_check_mark:" \
+    "Preview: ${PREVIEW_URL} \n Bacon: <${BACON_LINK}|${SHA}>"\
     "good"
